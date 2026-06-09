7c8f2b77b24b2a6152e01f329889edf893bb02ce
shahtaj gill <shahtajgill@gmail.com>
2026-06-09 17:05:43 -0400

    initial code for exercise

---
+++ starting-code/app/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/profile-picture', function (req, res) {
+  let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
+  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.end(img, 'binary');
+});
+
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000!");
+});
